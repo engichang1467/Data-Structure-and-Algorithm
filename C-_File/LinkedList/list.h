8c8c2ae25a404289a095fedb212a26e237eab737
Mike Chang <mchang0926@gmail.com>
2018-12-23 21:51:33 -0800

    Create list.h

---
+++ C-_File/LinkedList/list.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+class List
+{
+	private:
+		typedef struct node
+		{
+			int data;
+			node* next;
+		}* nodePtr;
+
+		nodePtr head;
+		nodePtr curr;
+		nodePtr temp;
+
+	public:  // This is where the functions go
+		List();
+		void AddNode(int addData);
+		void DeleteNode(int delData);
+		void PrintList();
+};
+
+#endif
